3587ebdf697dad205c16b587e5d1680802a9298e
RossieK <rosi.kirova96@gmail.com>
2021-02-26 20:42:11 +0200

    Add form toggling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,7 +29,7 @@ function App() {
   //Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task}
+    const newTask = { id, ...task }
     setTasks([...tasks, newTask])
   }
 
@@ -45,8 +46,8 @@ function App() {
 
   return (
     <div className="container" >
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      { showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks Yet'}
     </div>
   );
